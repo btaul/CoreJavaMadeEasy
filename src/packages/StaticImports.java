2217df4bcb350f08364cbbf0a97d1080fad20b23
Brenden Taul <89364653+btaul@users.noreply.github.com>
2022-05-30 13:15:47 -0500

    added packages assignment

---
+++ src/packages/StaticImports.java
@@ -0,0 +1,12 @@
+package packages;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+import static java.lang.System.out;
+
+public class StaticImports {
+
+    public static void main(String[] args){
+        out.println(sqrt(49));
+        out.println(pow(2, 4));
+    }
+}
